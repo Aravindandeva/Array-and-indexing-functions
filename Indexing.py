a8f8b201ba56cf31268162919e1a9fd5df3c5172
Aravindandeva <52481398+Aravindandeva@users.noreply.github.com>
2019-07-04 11:56:54 +0530

    Create Indexing.py

---
+++ Indexing.py
@@ -0,0 +1,14 @@
+a=int(input("INPUT"))
+if(a<0):
+  print("Positive")
+elif(a>0):
+  print("Negative")
+else:
+
+
+nam1=[1,2,3]
+nam2=[4,5,6]
+nam3=[7,8,9]
+list1=[nam1,nam2,nam3]
+for i in list1:
+  print(i[:])
